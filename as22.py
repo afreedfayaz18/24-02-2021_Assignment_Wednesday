10f227269e544649e3cf78f34ae3c2e1c9e6a83b
Muhammed Afreed Fayas <79002471+afreedfayaz18@users.noreply.github.com>
2021-02-24 13:48:47 +0530

    Add files via upload

---
+++ as22.py
@@ -0,0 +1,8 @@
+nlist1=[]
+list1=int(input("enter the elements :"))
+for i in range(list1):
+	nlist1.append(int(input("enter the values :")))
+tuple = tuple(nlist1)
+print('List : ',nlist1)
+print('Tuple : ',tuple)
+
