1f4735e518949a56723cd79ef4820e8fb91de0e1
Muhammed Afreed Fayas <79002471+afreedfayaz18@users.noreply.github.com>
2021-02-25 15:17:39 +0530

    Add files via upload

---
+++ as17.py
@@ -0,0 +1,7 @@
+sampleDict = {"name": "Kelly","age":25,"salary": 8000,"city": "New york"}
+print(sampleDict)
+new_key = "location"
+old_key = "city"
+sampleDict[new_key] = sampleDict.pop(old_key)
+print(sampleDict)
+
